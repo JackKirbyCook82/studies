9ee54293c4435efe24e7da8463ee757209b5fc3d
JackKirbyCook82 <jackcookchee@yahoo.com>
2020-07-01 17:14:54 -0700

    Updated

--- RentMarketEquilibrium/__init__.py
+++ RentMarketEquilibrium/__init__.py
@@ -12,7 +12,7 @@ from variables import Date, Geography
 from realestate.economy import Economy, Curve, Rate, Broker
 from realestate.households import Household
 from realestate.housing import Housing
-from realestate.markets import Personal_Property_Market, ConvergenceError
+from realestate.markets import Personal_Property_Market, Market_History, ConvergenceError
 
 pd.set_option('display.max_columns', 10)
 pd.set_option("display.precision", 0)
@@ -110,14 +110,14 @@ def createHousings(size, density, yearbuilts, sqfts, ranks, *args, prices, **kwa
         yield Housing.create(geography=geography, date=date, housing=dict(count=count, yearbuilt=yrblt, sqft=sqft, rank=rank, **housing), neighborhood=neighborhood, prices=prices)
                 
 def createMarket(*args, households, housings, income, yearbuilt, sqft, rank, **kwargs):
-    prices = dict(price=100000, rent=1000, sqftcost=0.5)    
+    prices = dict(price=100000, rent=500, sqftcost=0.5)    
     hhsizes, hhvalues = lornez(*args, **income, **kwargs)
     hgsizes, hgvalues = meshdistribution(*args, distributions=[yearbuilt, sqft, rank], **kwargs)
     ihouseholds = [ihousehold for ihousehold in createHouseholds(households, hhsizes, hhvalues, economy=economy)]
     ihousings = [ihousing for ihousing in createHousings(housings, hgsizes, *hgvalues, prices=prices)]
-    return Personal_Property_Market('renter', households=ihouseholds, housings=ihousings, rtol=0.002, atol=0.005)
+    return Personal_Property_Market('renter', households=ihouseholds, housings=ihousings, rtol=0.005, atol=0.01)
 
-def convergencePlot(dataframe, *args, colors, yearbuilt, sqft, rank, **kwargs):
+def historyPlot(dataframe, *args, colors, yearbuilt, sqft, rank, **kwargs):
     iyrblt, isqft, irank = np.meshgrid(*[np.arange(len(item['quantiles']) + 1) for item in (yearbuilt, sqft, rank,)])
     iyrblt, isqft, irank = [item.flatten() for item in (iyrblt, isqft, irank,)]
     colors = [colors['codes'][index] for index in dict(yearbuilt=iyrblt, sqft=isqft, rank=irank)[colors['key']]]
@@ -130,20 +130,22 @@ def convergencePlot(dataframe, *args, colors, yearbuilt, sqft, rank, **kwargs):
     
 def main(*args, **kwargs):
     market = createMarket(*args, **kwargs)
+    history = Market_History.fromMarket(market, *args, **kwargs)
     try: market(*args, economy=economy, broker=broker, **kwargs)    
-    except ConvergenceError: pass                
-    convergencePlot(market.convergence(), *args, **kwargs)
+    except ConvergenceError: pass          
+    historyPlot(history.table(), *args, **kwargs)
+    print(market.table(*args, **kwargs))
     print(Household.table())
     print(Housing.table())
 
 if __name__ == "__main__": 
     inputParms = {}
     inputParms['households'] = 1000
-    inputParms['housings'] = 900
-    inputParms['income'] = dict(average=75000/12, gini=0.3, quantiles=[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9], function=lornez_function, integral=lornez_integral)
+    inputParms['housings'] = 1000
+    inputParms['income'] = dict(average=50000/12, gini=0.3, quantiles=[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9], function=lornez_function, integral=lornez_integral)
     inputParms['yearbuilt'] = dict(lower=1950, upper=2010, quantiles=[0.333, 0.666], function=uniform_pdf)
     inputParms['sqft'] = dict(lower=1200, upper=3800, quantiles=[0.333, 0.666], function=uniform_pdf)
-    inputParms['rank'] = dict(lower=0, upper=100, quantiles=[0.25, 0.5, 0.75], function=uniform_pdf)
+    inputParms['rank'] = dict(lower=0, upper=100, quantiles=[0.333, 0.666], function=uniform_pdf)
     inputParms['colors'] = dict(key='sqft', codes=['r', 'g', 'b'])
     main(**inputParms)
 
